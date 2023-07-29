2362f7118606edef40aa3a61865657a030fd0806
saalse <12871822+saalse@users.noreply.github.com>
2023-07-30 03:47:56 +0800

    Chain multi ecosystem support added

--- blockchains.js
+++ blockchains.js
@@ -8,11 +8,26 @@ const renderBlockchains = async (sort, page = 1) => {
 
   if (!blockchains) {
     blockchains = await fetchAllFromAirtable('Blockchains');
+    console.log(blockchains);
     localStorage.setItem('blockchains', JSON.stringify(blockchains));
   }
 
   // Get all unique Ecosystem values
-  const ecosystems = [...new Set(blockchains.map((b) => b.fields.Ecosystem))];
+  const ecosystems = Array.from(
+    new Set(
+      blockchains.flatMap((b) => {
+        if (Array.isArray(b.fields.Ecosystem)) {
+          return b.fields.Ecosystem;
+        } else if (typeof b.fields.Ecosystem === 'string') {
+          return b.fields.Ecosystem.split(',').map((s) => s.trim());
+        } else {
+          console.error('Unexpected Ecosystem type:', b.fields.Ecosystem);
+          return [];
+        }
+      })
+    )
+  );
+
   const filterSelect = document.getElementById('filter-select');
 
   // Clear existing options
@@ -27,8 +42,8 @@ const renderBlockchains = async (sort, page = 1) => {
   // Add an option for each Ecosystem
   ecosystems.forEach((eco) => {
     const option = document.createElement('option');
-    option.value = eco ? eco : 'Undefined';
-    option.text = eco ? eco : 'Undefined';
+    option.value = eco;
+    option.text = eco;
     filterSelect.appendChild(option);
   });
 
@@ -46,9 +61,7 @@ const renderBlockchains = async (sort, page = 1) => {
 
     if (e.target.value !== 'All') {
       filteredBlockchains = blockchains.filter(
-        (b) =>
-          (b.fields.Ecosystem ? b.fields.Ecosystem : 'Undefined') ===
-          e.target.value
+        (b) => b.fields.Ecosystem && b.fields.Ecosystem.includes(e.target.value)
       );
     }
 
@@ -63,7 +76,11 @@ const renderBlockchains = async (sort, page = 1) => {
   if (filterValue !== 'All') {
     blockchains = blockchains.filter(
       (b) =>
-        (b.fields.Ecosystem ? b.fields.Ecosystem : 'Undefined') === filterValue
+        b.fields.Ecosystem &&
+        ((Array.isArray(b.fields.Ecosystem) &&
+          b.fields.Ecosystem.includes(filterValue)) ||
+          (typeof b.fields.Ecosystem === 'string' &&
+            b.fields.Ecosystem === filterValue))
     );
   }
 
@@ -80,11 +97,39 @@ const renderBlockchains = async (sort, page = 1) => {
   container.innerHTML = '';
 
   pagedBlockchains.forEach((blockchain) => {
+    console.log(blockchain); // <- Add this line
     const div = document.createElement('div');
     div.classList.add('p-3', 'border', 'rounded', 'mb-2');
-    const name = blockchain.fields.Name;
-    const ecosystem = blockchain.fields.Ecosystem; // Get Ecosystem value
-    div.textContent = ecosystem ? `${name} (${ecosystem})` : name; // Use Ecosystem value if it exists
+
+    const name = blockchain.fields ? blockchain.fields.Name : 'Undefined';
+    let ecosystem = 'Undefined';
+
+    if (blockchain.fields && Array.isArray(blockchain.fields.Ecosystem)) {
+      ecosystem = blockchain.fields.Ecosystem.join(', '); // Get Ecosystem value
+    } else if (
+      blockchain.fields &&
+      typeof blockchain.fields.Ecosystem === 'string'
+    ) {
+      try {
+        // Try to parse the string into an array
+        const parsedEcosystem = JSON.parse(blockchain.fields.Ecosystem);
+
+        if (Array.isArray(parsedEcosystem)) {
+          // If it's an array, join it into a string
+          ecosystem = parsedEcosystem.join(', ');
+        } else {
+          // If it's not an array, just use the string as is
+          ecosystem = blockchain.fields.Ecosystem;
+        }
+      } catch (err) {
+        // If parsing fails, just use the string as is
+        ecosystem = blockchain.fields.Ecosystem;
+      }
+    } else {
+      ecosystem = 'Undefined';
+    }
+
+    div.textContent = `${name} (${ecosystem})`; // Use Ecosystem value
     container.appendChild(div);
   });
 
